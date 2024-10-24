a28dfd4efbff06317aed895e87ff80772bd83701
shaminie <shaminie.s@hotmail.co.uk>
2024-10-24 20:25:47 +0100

    Finalised code for getting data and have finished downloading the data now

--- code/1-get-data.sh
+++ code/1-get-data.sh
@@ -2,28 +2,31 @@
 
 echo -e "\n retrieve data using curl from PubMed on the IDs of articles relating to COVID"
 
-curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=%22long%20covid%22&retmax=10000" > ../data/original/pmids.xml 
+curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=%22long%20covid%22&retmax=10000" > ../data/raw/pmids.xml 
 
 echo -e "\n what does  this data look like?"
-head ../data/original/pmids.xml
+head ../data/raw/pmids.xml
 
 echo -e "\n so there should be 6331 row, and en extra for the header"
 
 echo -e "\n edit dataset so that I just have a  list of IDs, save as csv and check the top of csv file"
-tail +5 ../data/original/pmids.xml | head -n-4 | sed 's/<Id>//g' | sed 's|[</Id>]||g' > ../data/derived/pmids.csv
-head  ../data/derived/pmids.csv
-rows=$(wc -l ../data/derived/pmids.csv | awk '{print $1}')
+tail +5 ../data/raw/pmids.xml | head -n-4 | sed 's/<Id>//g' | sed 's|[</Id>]||g' | sed 's/[[:space:]]//g'  > ../data/clean/pmids.csv
+head  ../data/clean/pmids.csv
+rows=$(wc -l ../data/clean/pmids.csv | awk '{print $1}')
 echo -e "\n there are $rows rows in the csv, one of which is a header"
 
 
 
 #use a for loop and the csv file to loop through and extract a file for each pmid
 
-
-for i in $(  eval echo "{1..$rows}")
+for i in $(  eval echo "{2..$rows}")
 do
-	pmid= $(sed '$ip'  ../data/derived/pmids.csv)
-	curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${pmid}" > ../data/original/article-data-$pmid.xml
-	echo  "extracted file number $i of $rows for article $pmid"
+	pmid=$(awk -F, "NR==$i {print $1}" ../data/clean/pmids.csv)
+	curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${pmid}" > ../data/raw/article-data-$pmid.xml
+	echo -e "\n extracted file number $i of $rows for article $pmid"
 	sleep 1
 done
+
+#check number of files in folder, should be 6332
+echo -e "\n  This number should be  66332"
+ls ../data/raw | wc -l
